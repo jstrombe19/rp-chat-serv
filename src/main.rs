7c8786362bb1219cbdb70fc5ee92260b37934df3
Jared Stromberg <jaredstromberg@gmail.com>
2021-09-22 13:06:53 -0600

    implemented Message and ServerMessage structs, incorporated serde_derive, defined incoming messages as JSON objects

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 use tokio::net::TcpListener;
 use tokio::prelude::*;
+use serde_derive::*;
 
 mod j_read;
 mod j_write;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message {
+  name: String,
+  tx: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ServerMessage {
+  mess: Option<Message>,
+  since: Option<usize>,
+}
 
 fn main() {
   let addr = "127.0.0.1:8088".parse().unwrap();
@@ -11,7 +24,8 @@ fn main() {
   let fut = lis.incoming().for_each(|sock| {
     let (sock_r, sock_w) = sock.split();
     let rd = j_read::JRead::new(sock_r).for_each(|s| {
-      println!("received: {}", s);
+      let v:ServerMessage = serde_json::from_str(&s)?;
+      println!("received: {:?}", v);
       Ok(())
     }).map_err(|_| ());
     tokio::spawn(rd);
